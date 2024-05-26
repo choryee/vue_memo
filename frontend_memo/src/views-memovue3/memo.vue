935db7415fd0a9e574d4f9d96ee6d9830980a8ba
김준형 <brain@insightinfo.kr>
2024-05-26 22:50:01 +0900

    memo.vue updated

--- frontend_memo/src/views-memovue3/memo.vue
+++ frontend_memo/src/views-memovue3/memo.vue
@@ -4,9 +4,13 @@
         <div class="btn-add">
             <button class="btn btn-primary" @click="add">+ 추가</button>
         </div>
+      <br/>
         <ul>
 <!--            <li v-for="(d,idx) in state.data" :key="idx" @click="edit(idx)" > {{ d }}</li>-->
-            <li v-for="d in state.data" :key="d.id" @click="edit(d.id)" > {{ d.content }}</li>
+            <li class="memo-li" v-for="d in state.data" :key="d.id" @click="edit(d.id)" >
+              <span>{{ d.content }}</span>
+              <button class="btn btn-warning">삭제</button>
+            </li>
         </ul>
         <Footer/>
     </div>
@@ -62,17 +66,30 @@ import axios from "axios";
 </script>
 
 <style lang="scss" scoped>
+*{
+  margin: 20px  20px 20px 20px;
+}
 .memo ul{
     list-style: none;
     padding: 15px 0;
     margin: 0;
     li{
-        padding: 15px;
-        margin: 5px;
+        //padding: 1px;
+        margin: 20px;
+        height: 20px;
+        //background-color: orange;
+        //border: 1px solid rebeccapurple;
     } 
 }
 .btn-add{
     text-align: right;
     padding: 10px 10px ;
-}   
+}
+.memo-li{
+  display:grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 20px;
+  //justify-content:space-between;
+}
+
 </style>
